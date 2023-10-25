c974e2d1ab939adc20f0c3ae4a7130b945800ddf
Santiago Saavedra <mssr.99@hotmail.com>
2023-10-25 16:13:18 -0300

    feat: Implemented api logic and passed Test #2

--- challenge/api.py
+++ challenge/api.py
@@ -1,4 +1,5 @@
 import fastapi
+from challenge.model import DelayModel
 
 app = fastapi.FastAPI()
 
@@ -9,5 +10,21 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(data: dict) -> dict:
+    try:
+        predicts = []
+        model = DelayModel()
+        flights = data["flights"]
+        for flight in flights:
+            if flight["TIPOVUELO"] not in ["N", "I"]:
+                raise Exception("Invalid TIPOVUELO")
+            if flight["MES"] not in range(1, 13):
+                raise Exception("Invalid MES")
+            features = model.preprocess(flight)
+            prediction = model.predict(features)
+            predicts.append(prediction[0])
+        return {
+            "predict": predicts
+        }
+    except Exception as e:
+        return fastapi.Response(content=str(e), status_code=400)
